a7f23fe3f830cc5cc3eb87b23e9306933c2a22e6
Nikita Koritsky <nikita.koritsky@gmail.com>
2019-09-30 16:57:21 +0300

    first push of my site

---
+++ single.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7f660904bd.js" crossorigin="anonymous"></script>
+    <title>Geopon</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler ml-auto mr-auto" type="button" data-toggle="collapse" data-target="#navbar"
+                aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav text-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#link-1">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#link-2">Услуги</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#link-3">О нас</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#link-4">Контакты</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-4 map-mark">
+            <i class="fas fa-map-marked-alt "></i>
+            <span class="town"> г. Запорожье<br>ул.Новокузнецкая21 </span>
+        </div>
+        <div class="col-4">
+            <img src="./img/geologo.png" alt="logo">
+        </div>
+        <div class="col-4 phone-numer">
+            <i class="fas fa-phone-square-alt "></i>
+            <br>
+            <span class="numer">+380 96 5396848<br>+380 99 0781841</span>
+        </div>
+    </div>
+</div>
+<div class="main-image">
+    <img src="./img/geodesy-700x310.jpg" alt="content-image">
+</div>
+</body>
+</html>
+
+<?php
