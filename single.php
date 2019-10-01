26aaafdd5476a19f67eed1564536cc0700ba7cbc
Discoihor <piv.disco@gmail.com>
2019-10-01 15:28:01 +0300

    test update

--- single.php
+++ single.php
@@ -51,7 +51,7 @@
             <i class="fas fa-map-marked-alt "></i>
             <span class="town"> г. Запорожье<br>ул.Новокузнецкая21 </span>
         </div>
-        <div class="col-4">
+        <div class="col-4 col-img">
             <img src="./img/geologo.png" alt="logo">
         </div>
         <div class="col-4 phone-numer">
@@ -63,6 +63,89 @@
 </div>
 <div class="main-image">
     <img src="./img/geodesy-700x310.jpg" alt="content-image">
+</div>
+<div class="service">
+    <span>Наши услуги:</span>
+</div>
+
+<div class="container content-tittle-geodesy">
+    <span class="content-first-tittle">Геодезия: </span>
+</div>
+<div class="container main-content">
+    <div class="row">
+        <div class="col-4 main-content-geodesy">
+            <span class="content-tittle-first-geodesy">1</span>
+            <h4>ТОПОГРАФИЧЕСКИЕ РАБОТЫ</h4>
+            <p>Топографическая съемка -</p>
+        </div>
+        <div class="col-4 main-content-geodesy2">
+            <span class="content-tittle-second-geodesy">2</span>
+            <h4>КАДАСТРОВЫЕ РАБОТЫ</h4>
+            <p>Изготовление схемы -</p>
+        </div>
+        <div class="col-4 main-content-geodesy3">
+            <span class="content-tittle-third-geodesy">3</span>
+            <h4>ВЫНОС ГРАНИЦ УЧАСТКА</h4>
+            <p>Вынос границ участка в натуру -</p>
+        </div>
+    </div>
+</div>
+
+<div class="container content-tittle">
+    <span class="content-first-tittle">Землеустройство: </span>
+</div>
+<div class="container main-content">
+    <div class="row">
+        <div class="col-4 main-content-land-management">
+            <span class="content-tittle-first">1</span>
+            <h4>РАЗРАБОТКА ТЕХНИЧЕС-КОЙ ДОКУМЕНТАЦИИ</h4>
+            <p>Техническая документация по установлению границ разрабатывается для: определения на местности границ
+                участка, внесения в государственный земельный кадастр данных об участке, присвоения кадастрового номера,
+                исправления ошибок -</p>
+        </div>
+        <div class="col-4 main-content-land-management2">
+            <span class="content-tittle-second">2</span>
+            <h4>ПРОЕКТ ОТВОДА ЗЕМЕЛЬНОГО УЧАСТКА</h4>
+            <p>Ключевой этап при оформлении права собственности и приватизации земли -</p>
+        </div>
+        <div class="col-4 main-content-land-management3">
+            <span class="content-tittle-third">3</span>
+            <h4>РАЗДЕЛЕНИЕ/ОБЪЕДИ-НЕНИЕ УЧАСТКА</h4>
+            <p>Для разделения / объединения участка готовится техническая документация. Основание для подготовки:
+                нотариальное заявление собственника, договор, решение суда, или органов власти о разделе -</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4 main-content-land-management4">
+            <span class="content-tittle-force">4</span>
+            <h4>СМЕНА ЦЕЛЕВОГО НАЗНАЧЕНИЯ УЧАСТКА</h4>
+            <p>Для использования земли согласно своим нуждам, иногда необходима смена целевого назначения земельного
+                участка. Смена назначения участка оформляется путём разработки проекта землеустройства -</p>
+        </div>
+        <div class="col-4 main-content-land-management5">
+            <span class="content-tittle-fives">5</span>
+            <h4>ПРИСВОЕНИЕ КАДАСТРОВОГО НОМЕРА </h4>
+            <p>Для покупки, продажи, обмена, получения в наследство земельного участка, согласно законодательству
+                Украины - нужно присвоить участку кадастровый номер -</p>
+        </div>
+        <div class="col-4 main-content-land-management6">
+            <span class="content-tittle-six">6</span>
+            <h4>РЕГИСТРАЦИЯ ПРАВ СОБСТВЕННОСТИ</h4>
+            <p>Для начала/окончания застройки, капитального ремонта или реконструкции частного дома или другого объекта
+                1-3 категории сложности - следует получить документы, дающие разрешение на выполнение вышеуказанных
+                работ, а именно декларации о начале/конце строительства. -</p>
+        </div>
+    </div>
+</div>
+<div class="main-image2">
+    <img src="./img/geologo2.png" alt="content-image">
+</div>
+<div class="about-us">
+    <h2>О нас</h2>
+    <p>ЧП "Пономаренко"(код ЕГРПОУ ********) лидирует на рынке более десяти лет. Мы привыкли достигать результатов
+        раньше остальных, покоряя своих клиентов качественной работой, гибкими ценами и кратчайшими сроками. У нас
+        работают самые опытные специалисты, чей суммарный стаж работы в сфере землеустройства и геодезии составляет
+        более сорока лет.</p>
 </div>
 </body>
 </html>
